09297ce0affe6639b20c7b36a8617a743db869fc
Bakazova Aida <solnyshko669@gmail.com>
2022-03-16 00:12:35 +0600

    correct

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
+// const allSmallImages = document.querySelectorAll('.small-img')
+// const bigImg = document.querySelector('.big-img img')
+//
+//
+//
+// allSmallImages.forEach((item) => {
+//     item.addEventListener('click', () => {
+//         bigImg.src = item.children[0].src
+//         allSmallImages.forEach(oneWrapper => oneWrapper.classList.remove('active'))
+//         item.classList.add('active')
+//     })
+// })
+// bigImg.addEventListener('mousemove', (e) => {
+//     bigImg.style.transform = `scale(2) translate(${e.offsetX / 10}px,${e.offsetY / 10})`
+//
+// })
+
 const allSmallImages = document.querySelectorAll('.small-img')
 const bigImg = document.querySelector('.big-img img')
 
 
-
 allSmallImages.forEach((item) => {
     item.addEventListener('click', () => {
-        bigImg.src = item.src
+        console.log(item.src)
     })
-})
-bigImg.addEventListener('mousemove', (e) => {
-    bigImg.style.transform = "scale(2)"
-    bigImg.style.transform = `translateX(${e.offsetX}px)`
 })
