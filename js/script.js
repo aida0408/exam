0b5951319d746c11293a37bcbaab4e35118acd02
Bakazova Aida <solnyshko669@gmail.com>
2022-03-16 01:18:11 +0600

    end

--- js/script.js
+++ js/script.js
@@ -1,26 +1,21 @@
-// const allSmallImages = document.querySelectorAll('.small-img')
-// const bigImg = document.querySelector('.big-img img')
-//
-//
-//
-// allSmallImages.forEach((item) => {
-//     item.addEventListener('click', () => {
-//         bigImg.src = item.children[0].src
-//         allSmallImages.forEach(oneWrapper => oneWrapper.classList.remove('active'))
-//         item.classList.add('active')
-//     })
-// })
-// bigImg.addEventListener('mousemove', (e) => {
-//     bigImg.style.transform = `scale(2) translate(${e.offsetX / 10}px,${e.offsetY / 10})`
-//
-// })
 
-const allSmallImages = document.querySelectorAll('.small-img')
+
+const allSmallImages = document.querySelectorAll('.small-wrapper')
 const bigImg = document.querySelector('.big-img img')
+
 
 
 allSmallImages.forEach((item) => {
     item.addEventListener('click', () => {
-        console.log(item.src)
+        bigImg.src = item.children[0].src
+        allSmallImages.forEach(oneWrapper => oneWrapper.classList.remove('active'))
+        item.classList.add('active')
     })
+})
+
+bigImg.addEventListener('mousemove', (e) => {
+    bigImg.style.transform = `scale(2) translate(${e.offsetX /10}px,${e.offsetY / 10}px)`
+})
+bigImg.addEventListener('mouseout', (e) => {
+    bigImg.style.transform = `scale(1) translate(0, 0)`
 })
